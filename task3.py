8d61fc1bc6e3ecdd251732f43e75c63df9491158
dias <dias_tashev@protonmail.com>
2021-12-18 13:52:18 +0600

    little changes in design

--- task3.py
+++ task3.py
@@ -3,6 +3,5 @@ print('"n + nn + nnn" counter')
 n = input('input your n: ')
 nn = 2 * n
 nnn = 3 * n
-
-sum = int(n) + int(nn) + int(nnn)
-print('your result is: ' + str(sum))
+sum = (int(n) + int(nn) + int(nnn))
+print('{0} + {1} + {2} = {3}'.format(n, nn, nnn, sum))
